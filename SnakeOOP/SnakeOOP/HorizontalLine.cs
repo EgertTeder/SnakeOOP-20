f0e20f926f3668dfbe0182d264a1668112028b39
Opilane TTHK <opilane@tthk.ee>
2022-01-04 12:40:17 +0200

    Snake

---
+++ SnakeOOP/SnakeOOP/HorizontalLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class HorizontalLine
+    {
+        List<Point> pointList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symb)
+        {
+            pointList = new List<Point>();
+
+            for(int i = xLeft; i <= xRight; i++)
+            {
+                Point p = new Point(i, y, symb);
+                pointList.Add(p);
+            }
+
+            //##########
+
+           
+        }
+        public void Draw()
+        {
+            foreach(Point point in pointList)
+            {
+                Console.SetCursorPosition(point.x, point.y);
+                Console.Write(point.symb);
+            }
+        }
+    }
+}
---
+++ SnakeOOP/SnakeOOP/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class VerticalLine
+    {
+        List<Point> pointList;
+
+        public VerticalLine(int yUp, int yDown, int x, char symp)
+        {
+            pointList = new List<Point>();
+            for(int i = yUp; i < yDown; i++)
+            {
+                Point p = new Point(x, i, symb);
+                pointList.Add(p);
+
+            }
+
+        }
+
+        public void Draw()
+        {
+            foreach(Point point in pointList)
+            {
+                point.Draw();
+            }
+        }
+    }
+}
